db682bf9217b2995352df237178ff2c7bf6d59cf
Builder v6.5 <builder@mforge.com.br>
2026-06-03 17:18:27 -0300

    Migrate convite: JulianaBusson35Anos

---
+++ JulianaBusson35Anos/config.js
@@ -0,0 +1,86 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Juliana Busson",
+    "idade": 15,
+    "data": "2026-05-22",
+    "hora": "20:00",
+    "endereco": ""
+  },
+  "convite": {
+    "paletaCores": "Azul e Dourado",
+    "tema": "Bridgerton",
+    "musica": "assets/music_1780517906335.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "JulianaBusson35Anos",
+    "seo": {
+      "pageTitle": "Juliana Busson| 35 Anos",
+      "ogTitle": "Você está convidado para os 35 anos da Juliana!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1780517906335.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1780517906335.mp4",
+      "assets/slide2_1780517906335.jpg",
+      "assets/slide3_1780517906335.mp4",
+      "assets/slide4_1780517906335.mp4",
+      "assets/slide5_1780517906335.jpg"
+    ],
+    "folhaVazia": "assets/folha_1780517906335.jpg",
+    "folhaPreenchida": "assets/folha_1780517906335.jpg",
+    "musica": "assets/music_1780517906335.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {
+      "1": "assets/66324177-dbf7-47b1-9fdb-2f41fe9ec969_1780517906335.jpg"
+    }
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/q1N9GmGCkHk8aWMXA"
+    },
+    {
+      "tipoAcao": "PopupImagem",
+      "tipoVisual": "css",
+      "titulo": "Dicas de Presente",
+      "icone": "fa-solid fa-gift",
+      "conteudo": "assets/66324177-dbf7-47b1-9fdb-2f41fe9ec969_1780517906335.jpg"
+    },
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5585985924943",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#3e6084",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
