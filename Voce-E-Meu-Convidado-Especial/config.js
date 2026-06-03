6e9617e6dac59ba526aa5bc695aedd6aa57db3c4
Builder v6.5 <builder@mforge.com.br>
2026-06-03 17:20:57 -0300

    Migrate convite: Voce-E-Meu-Convidado-Especial

---
+++ Voce-E-Meu-Convidado-Especial/config.js
@@ -0,0 +1,73 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Gabi",
+    "idade": 15,
+    "data": "2026-09-19",
+    "hora": "19:00",
+    "endereco": "Av. Manoel Ribas, 6860 - Santa Felicidade, Curitiba - PR, 82400-000\n"
+  },
+  "convite": {
+    "paletaCores": "",
+    "tema": "",
+    "musica": "assets/music_1780518056175.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "Voce-E-Meu-Convidado-Especial",
+    "seo": {
+      "pageTitle": "Gabi - 15 Anos",
+      "ogTitle": "Você está convidado para os 15 anos da Gabi!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1780518056175.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1780518056175.mp4"
+    ],
+    "folhaVazia": "assets/folha_1780518056175.jpg",
+    "folhaPreenchida": "assets/folha_1780518056175.jpg",
+    "musica": "assets/music_1780518056175.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/SjM8MV5yQoQhmyX69?g_st=iw"
+    },
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5541996643094",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": true
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#0c2b71",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
