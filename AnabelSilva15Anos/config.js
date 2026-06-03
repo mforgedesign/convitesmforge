f43200de4bddcd246abc21751faf30f17363b884
Builder v6.5 <builder@mforge.com.br>
2026-06-03 17:14:44 -0300

    Migrate convite: AnabelSilva15Anos

---
+++ AnabelSilva15Anos/config.js
@@ -0,0 +1,79 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Anabel Silva",
+    "idade": 18,
+    "data": "2026-12-20",
+    "hora": "20:00",
+    "endereco": "Av. Maria Campos, 464 - Centro, Osasco - SP, 06010-065"
+  },
+  "convite": {
+    "paletaCores": "Azul com Prata",
+    "tema": "15 Anos",
+    "musica": "assets/music_1780517683875.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "AnabelSilva15Anos",
+    "seo": {
+      "pageTitle": "Convite: 18 Anos - Anabel Silva",
+      "ogTitle": "Você está convidado para os 18 anos da Anabel Silva!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1780517683875.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1780517683875.mp4"
+    ],
+    "folhaVazia": "assets/folha_1780517683875.jpg",
+    "folhaPreenchida": "assets/folha_1780517683875.jpg",
+    "musica": "assets/music_1780517683875.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/KovUiTzz2Szr9BVn6"
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Dicas de Presente",
+      "icone": "fa-solid fa-gift",
+      "conteudo": "https://aniversario-wagl.listaideal.com.br/pt"
+    },
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5511977186185",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": true
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "buttonColor": "#441214",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
