b908f5ad9e9ad988499669285a326294294b07d0
Builder v6.5 <builder@mforge.com.br>
2026-06-03 17:15:50 -0300

    Migrate convite: annyeclara15anos

---
+++ annyeclara15anos/config.js
@@ -0,0 +1,77 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Annye Clara",
+    "idade": 15,
+    "data": "2026-06-21",
+    "hora": "16:30",
+    "endereco": "Buffet Dream Garden Rua Borcelle, 312 - Brasilândia São Paulo - SP"
+  },
+  "convite": {
+    "paletaCores": "Azul com Prata",
+    "tema": "15 Anos",
+    "musica": "assets/music_1780517750073.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "annyeclara15anos",
+    "seo": {
+      "pageTitle": "Convite: 15 Anos - Annye Clara",
+      "ogTitle": "Você está convidado para os 15 anos da Annye Clara!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1780517750073.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1780517750073.mp4",
+      "assets/slide2_1780517750073.mp4",
+      "assets/slide3_1780517750073.mp4",
+      "assets/slide4_1780517750073.jpg",
+      "assets/slide5_1780517750073.mp4",
+      "assets/slide6_1780517750073.mp4"
+    ],
+    "folhaVazia": "assets/folha_1780517750073.jpg",
+    "folhaPreenchida": "assets/folha_1780517750073.jpg",
+    "musica": "assets/music_1780517750073.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/GjZr864wroKsjpSh7"
+    },
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "554196536414",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": true
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "buttonColor": "#4593b5",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
