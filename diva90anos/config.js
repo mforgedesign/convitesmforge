23326dd612ee61db66808d24c32c87e6c2bf823a
Builder v6.5 <builder@mforge.com.br>
2026-06-03 17:16:48 -0300

    Migrate convite: diva90anos

---
+++ diva90anos/config.js
@@ -0,0 +1,73 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Diva Vidal",
+    "idade": 90,
+    "data": "2026-06-06",
+    "hora": "13:00",
+    "endereco": "Rua Flavita Bretas, 226 salão de festas - Luxemburgo"
+  },
+  "convite": {
+    "paletaCores": "verde, lilás, dourado",
+    "tema": "",
+    "musica": "assets/music_1780517807642.mp3",
+    "tipoAbertura": "curta",
+    "particulasAbertura": true,
+    "slug": "diva90anos",
+    "seo": {
+      "pageTitle": "Convite: Aniversario 90 anos - Diva Vidal",
+      "ogTitle": "Você está convidado para o evento de Diva Vidal!",
+      "ogDescription": "Embarque nessa experiência mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/cover_1780517807642.jpg",
+    "aberturaSlides": [
+      "assets/slide1_1780517807642.mp4"
+    ],
+    "folhaVazia": "assets/folha_1780517807642.jpg",
+    "folhaPreenchida": "assets/folha_1780517807642.jpg",
+    "musica": "assets/music_1780517807642.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "ConfirmarPresenca",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5511940820140",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/ooL1AN9WTAoXh33eA"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#6a2c7a",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
