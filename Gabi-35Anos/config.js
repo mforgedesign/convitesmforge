7c58244aeb954fde330c0866a6f62ff6defaadcf
Builder v6.5 <builder@mforge.com.br>
2026-06-09 14:01:31 -0300

    Deploy convite: Gabi-35Anos

---
+++ Gabi-35Anos/config.js
@@ -0,0 +1,66 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Gabi",
+    "idade": 15,
+    "data": "2026-07-18",
+    "hora": "20:00",
+    "endereco": ""
+  },
+  "convite": {
+    "paletaCores": "",
+    "tema": "",
+    "musica": "",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "Gabi-35Anos",
+    "seo": {
+      "pageTitle": "Você recebeu um convite especial!",
+      "ogTitle": "Você recebeu um convite especial!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "",
+    "aberturaSlides": [],
+    "folhaVazia": "assets/folha_1781024491044.jpg",
+    "folhaPreenchida": "assets/folha_1781024491044.jpg",
+    "musica": "",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/LULUdwpmBnw98bYx9"
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "conteudo": "https://docs.google.com/forms/d/e/1FAIpQLSeOICm_KiX8VNgI_HxUW06p4O0KsO_v4GDJGXhtP6grsOGx8Q/viewform?usp=dialog"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#e42d7c",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
