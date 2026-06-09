ceee8cfe8b899445c740b48b68c379203f7289ef
Builder v6.5 <builder@mforge.com.br>
2026-06-09 14:05:09 -0300

    Deploy convite: Gabi-35-Anos

---
+++ Gabi-35-Anos/config.js
@@ -0,0 +1,45 @@
+window.config = {
+  "productType": "savethedate",
+  "convite": {
+    "slug": "Gabi-35-Anos",
+    "musica": "",
+    "seo": {
+      "pageTitle": "Save The Date!",
+      "ogTitle": "Save The Date!",
+      "ogDescription": "Reserve essa data especial!"
+    }
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/LULUdwpmBnw98bYx9"
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "conteudo": "https://docs.google.com/forms/d/e/1FAIpQLSeOICm_KiX8VNgI_HxUW06p4O0KsO_v4GDJGXhtP6grsOGx8Q/viewform?usp=dialog"
+    }
+  ],
+  "assets": {
+    "folhaPreenchida": "assets/folha_1781024709352.jpg",
+    "musica": "",
+    "folhaVazia": "assets/folha_1781024709352.jpg"
+  },
+  "config": {
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    },
+    "botoes": {
+      "cor": "#ffffff",
+      "tamanho": 1,
+      "preenchido": true
+    }
+  }
+};
