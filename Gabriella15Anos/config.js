0f3c4565e55d8a93a14e089322c57366691924c9
Builder v6.5 <builder@mforge.com.br>
2026-06-23 18:34:21 -0300

    Deploy convite: Gabriella15Anos

--- Gabriella15Anos/config.js
+++ Gabriella15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Marsala",
     "tema": "Floral",
-    "musica": "assets/music_1782250407902.mp3",
+    "musica": "assets/music_1782250460865.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Gabriella15Anos",
@@ -25,22 +25,22 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1782250407902.jpg",
-    "cartao": "assets/cartao_gen_1782250407902.jpg",
+    "capa": "assets/cover_1782250460865.jpg",
+    "cartao": "assets/cartao_gen_1782250460865.jpg",
     "aberturaSlides": [
-      "assets/slide1_1782250407902.mp4",
-      "assets/slide2_1782250407902.jpg",
-      "assets/slide3_1782250407902.mp4",
-      "assets/slide4_1782250407902.jpg"
+      "assets/slide1_1782250460865.mp4",
+      "assets/slide2_1782250460865.jpg",
+      "assets/slide3_1782250460865.mp4",
+      "assets/slide4_1782250460865.jpg"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1782250407902.mp4",
+        "filePath": "assets/slide1_1782250460865.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1782250407902.jpg",
+        "filePath": "assets/slide2_1782250460865.jpg",
         "label": "Slide 2",
         "type": "video"
       },
@@ -50,14 +50,14 @@ window.config = {
         "type": "video"
       },
       {
-        "filePath": "assets/slide4_1782250407902.jpg",
+        "filePath": "assets/slide4_1782250460865.jpg",
         "label": "Slide 4",
         "type": "video"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1782250407902.jpg",
-    "folhaPreenchida": "assets/folha_1782250407902.jpg",
-    "musica": "assets/music_1782250407902.mp3",
+    "folhaVazia": "assets/folha_vazia_1782250460865.jpg",
+    "folhaPreenchida": "assets/folha_1782250460865.png",
+    "musica": "assets/music_1782250460865.mp3",
     "fotos": [],
     "popupImagensPorBotao": {}
   },
