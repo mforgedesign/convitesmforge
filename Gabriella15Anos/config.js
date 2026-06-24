8cd8b91e17ab06b71a2d0b087a331a8df18666d5
Builder v6.5 <builder@mforge.com.br>
2026-06-24 13:17:46 -0300

    Deploy convite: Gabriella15Anos

--- Gabriella15Anos/config.js
+++ Gabriella15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Marsala",
     "tema": "Floral",
-    "musica": "assets/music_1782250460865.mp3",
+    "musica": "assets/music_1782317865576.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Gabriella15Anos",
@@ -25,39 +25,51 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1782250460865.jpg",
-    "cartao": "assets/cartao_gen_1782250460865.jpg",
+    "capa": "assets/cover_1782317865576.jpg",
+    "cartao": "assets/cartao_gen_1782317865576.jpg",
     "aberturaSlides": [
-      "assets/slide1_1782250460865.mp4",
-      "assets/slide2_1782250460865.jpg",
-      "assets/slide3_1782250460865.mp4",
-      "assets/slide4_1782250460865.jpg"
+      "assets/slide1_1782317865576.mp4",
+      "assets/slide2_1782317865576.jpg",
+      "assets/slide3_1782317865576.mp4",
+      "assets/slide4_1782317865576.jpg"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1782250460865.mp4",
+        "filePath": "assets/slide1_1782317865576.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1782250460865.jpg",
+        "filePath": "assets/slide2_1782317865576.jpg",
         "label": "Slide 2",
         "type": "video"
       },
       {
-        "filePath": "assets/slide3_cena.mp4",
+        "filePath": "assets/slide3_1782317865576.mp4",
         "label": "Slide 3",
         "type": "video"
       },
       {
-        "filePath": "assets/slide4_1782250460865.jpg",
+        "filePath": "assets/slide4_1782317865576.jpg",
         "label": "Slide 4",
         "type": "video"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1782250460865.jpg",
-    "folhaPreenchida": "assets/folha_1782250460865.png",
-    "musica": "assets/music_1782250460865.mp3",
+    "folhaVazia": "assets/folha_vazia_1782317865576.jpg",
+    "folhaPreenchida": "assets/folha_1782317865576.jpg",
+    "musica": "assets/music_1782317865576.mp3",
+    "fabric": {
+      "saveTheDate": {
+        "referencia": "",
+        "resultado": "",
+        "musicStartSec": 0
+      },
+      "lembrete": {
+        "referencia": "",
+        "resultado": "",
+        "musicStartSec": 0
+      }
+    },
     "fotos": [],
     "popupImagensPorBotao": {}
   },
