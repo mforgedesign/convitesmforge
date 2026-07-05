6282d526765704a98981748ce9b3b9ef7a6380b8
Builder v6.5 <builder@mforge.com.br>
2026-07-05 17:46:28 -0300

    Deploy convite: Thalita15Anos

--- Thalita15Anos/config.js
+++ Thalita15Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "",
     "tema": "",
-    "musica": "assets/music_1783284070827.mp3",
+    "musica": "assets/music_1783284388131.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Thalita15Anos",
@@ -25,27 +25,27 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1783284070827.jpg",
-    "cartao": "assets/cartao_gen_1783284070827.jpg",
+    "capa": "assets/cover_1783284388131.jpg",
+    "cartao": "assets/cartao_gen_1783284388131.jpg",
     "aberturaSlides": [
-      "assets/slide1_1783284070827.mp4",
-      "assets/slide2_1783284070827.mp4"
+      "assets/slide1_1783284388131.mp4",
+      "assets/slide2_1783284388131.mp4"
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1783284070827.mp4",
+        "filePath": "assets/slide1_1783284388131.mp4",
         "label": "Abertura (Video)",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1783284070827.mp4",
+        "filePath": "assets/slide2_1783284388131.mp4",
         "label": "Gerando...",
         "type": "image"
       }
     ],
-    "folhaVazia": "",
-    "folhaPreenchida": "assets/folha_1783284070827.jpg",
-    "musica": "assets/music_1783284070827.mp3",
+    "folhaVazia": "assets/folha_vazia_1783284388131.jpg",
+    "folhaPreenchida": "assets/folha.jpg",
+    "musica": "assets/music_1783284388131.mp3",
     "fabric": {
       "saveTheDate": {
         "referencia": "",
