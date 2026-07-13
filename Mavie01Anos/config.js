07c08447a09068c2e0d2e7a39be6f162ceadce4e
Builder v6.5 <builder@mforge.com.br>
2026-07-13 10:26:27 -0300

    Deploy convite: Mavie01Anos

--- Mavie01Anos/config.js
+++ Mavie01Anos/config.js
@@ -14,7 +14,7 @@ window.config = {
   "convite": {
     "paletaCores": "Rosa e Rosé Gold",
     "tema": "Jardim Encantado",
-    "musica": "assets/music_1783946580145.mp3",
+    "musica": "assets/music_1783949187338.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Mavie01Anos",
@@ -25,28 +25,28 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1783946580145.jpg",
-    "cartao": "assets/cartao_chatgpt_1783946580145.jpg",
+    "capa": "assets/cover_1783949187338.jpg",
+    "cartao": "assets/cartao_chatgpt_1783949187338.jpg",
     "aberturaSlides": [
-      "assets/slide1_1783946580145.mp4",
-      "assets/slide2_1783946580145.mp4",
-      "assets/slide3_1783946580145.mp4",
+      "assets/slide1_1783949187338.mp4",
+      "assets/slide2_1783949187338.mp4",
+      "assets/slide3_1783949187338.mp4",
       "",
       ""
     ],
     "aberturaSlidesMeta": [
       {
-        "filePath": "assets/slide1_1783946580145.mp4",
+        "filePath": "assets/slide1_1783949187338.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide2_1783946580145.mp4",
+        "filePath": "assets/slide2_1783949187338.mp4",
         "label": "Slide 1",
         "type": "video"
       },
       {
-        "filePath": "assets/slide3_1783946580145.mp4",
+        "filePath": "assets/slide3_1783949187338.mp4",
         "label": "Slide 2",
         "type": "video"
       },
@@ -61,9 +61,9 @@ window.config = {
         "type": "video"
       }
     ],
-    "folhaVazia": "assets/folha_vazia_1783946580145.jpg",
-    "folhaPreenchida": "assets/folha_1783946580145.png",
-    "musica": "assets/music_1783946580145.mp3",
+    "folhaVazia": "assets/folha_vazia_1783949187338.jpg",
+    "folhaPreenchida": "assets/folha_1783949187338.png",
+    "musica": "assets/music_1783949187338.mp3",
     "fabric": {
       "saveTheDate": {
         "referencia": "",
